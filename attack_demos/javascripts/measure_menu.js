2bbbc17ff42bd2bccf9217e38f35e3533131b576
xlin29 <38230764+xlin29@users.noreply.github.com>
2021-03-09 00:19:46 -0600

    Delete measure_menu.js

--- attack_demos/javascripts/measure_menu.js
+++
@@ -1,8 +0,0 @@
-const webdriver = require('selenium-webdriver');
-const chromium = require('/Users/linangelcat/Downloads/drivers/chromedriver1');
-
-let driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(/* ... */)
-    .setFirefoxOptions(/* ... */)
-    .build();
